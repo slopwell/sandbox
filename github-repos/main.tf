5d955f18eb9bb7a09e93a94ebd0923efb13e6818
slopwell <210498326+slopwell@users.noreply.github.com>
2025-10-12 10:29:29 +0900

    リポジトリ追加

--- github-repos/main.tf
+++ github-repos/main.tf
@@ -8,3 +8,13 @@ module "web_framework_n_knocks" {
   visibility      = "public"
   topics          = ["web-framework", "learning"]
 }
+
+module "pts_viewer" {
+  source = "./modules/repository"
+
+  github_token    = var.github_token
+  github_owner    = var.github_owner
+  repository_name = "pts-viewer"
+  description     = "共通棚割ファイルの可視化ツール"
+  visibility      = "public"
+}
