58b0fac00aae8fc27e52d54c50ff5223c2d6b992
slopwell <210498326+slopwell@users.noreply.github.com>
2025-11-09 22:05:34 +0900

    feat:cloudfront側の実装(output忘れ

--- cloudfron-x-jwt/terraform/stack/large-file-storage/providers.tf
+++ cloudfron-x-jwt/terraform/stack/large-file-storage/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.11.0"
+      source                = "hashicorp/aws"
+      version               = ">= 5.11.0"
       configuration_aliases = [aws.us_east_1]
     }
     archive = {
